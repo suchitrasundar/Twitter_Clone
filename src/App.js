d8499390c347d84942fc96e512fc77e69391d9ee
suchitrasundar <suchitrasun@gmail.com>
2021-05-08 19:02:51 -0500

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>Hey</h1>
+
+      {/*Sidebar */}
+      <Sidebar />
+      {/* Twitter Feed*/}
+      {/* Widget */}
+
     </div>
   );
 }
